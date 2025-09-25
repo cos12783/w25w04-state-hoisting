231332fa5f8bb950dcc1717707224040e180b9a1
cos12783 <cos12783@naver.com>
2025-09-25 16:34:52 +0900

    🚀총합 출력

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,32 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
 
   return (
     <div>
-      <Counter count={count} 
+      <h1>총합: {count1 + count2}</h1>
+      
+      <Counter count={count1} 
       onIncrement={
-        () => setCount(prev => prev + 1)
-      }/>
-
-      <Counter count={count} 
+        () => setCount1(prev => prev + 1)
+        } />
+      
+      <Counter count={count2} 
       onIncrement={
-        () => setCount(prev => prev + 1)
-      }/>
-      {/* <Counter/> */}
+        () => setCount2(prev => prev + 1)
+        } />
     </div>
   )
 }
 
-function Counter() {{count, onIncrement}  
+function Counter({ count, onIncrement }) {
   return (
     <div>
       <h1>Counter: {count}</h1>
-      <button 
-        onClick={onIncrement}>
-          증가
+      <button onClick={onIncrement}>
+        증가
       </button>
     </div>
   )
